c4ab13d031adb2c178867a65cfee6c70dc8f00ee
Matthew Davies <mattjdavies199@gmail.com>
2023-04-13 15:24:39 -0600

    Bootstrap

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -131,7 +131,10 @@ namespace INTEX.Controllers
         //[Authorize(Roles ="Admin")]
 
 
-
+        public IActionResult Unsupervised()
+        {
+            return View();
+        }
 
 
         public IActionResult Privacy()
